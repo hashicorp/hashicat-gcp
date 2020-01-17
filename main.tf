9f246a7c87d53f9f66f40fca634182d25a4b0ceb
Sean Carolan <scarolan@hashicorp.com>
2020-01-17 15:38:29 -0600

    Hashicat GCP is aliiiiive 🐈

--- main.tf
+++ main.tf
@@ -1,30 +1,31 @@
 provider "google" {
-  project = "hashicat-gcp-dev"
-  region  = "us-central1"
-  zone    = "us-central1-a"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
-resource "google_compute_firewall" "allow-external-ssh" {
-  name    = "allow-external-ssh"
+resource "tls_private_key" "ssh-key" {
+  algorithm = "RSA"
+  rsa_bits  = "4096"
+}
+
+resource "google_compute_firewall" "http-server" {
+  name    = "default-allow-http"
   network = "default"
 
   allow {
     protocol = "tcp"
-    ports    = ["22"]
+    ports    = ["80"]
   }
 
+  // Allow traffic from everywhere to instances with an http-server tag
   source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["externalssh"]
+  target_tags   = ["http-server"]
 }
 
-resource "tls_private_key" "ssh-key" {
-  algorithm   = "ECDSA"
-  ecdsa_curve = "P384"
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
+resource "google_compute_instance" "hashicat" {
+  name         = "${var.prefix}-hashicat"
+  machine_type = var.machine_type
 
   boot_disk {
     initialize_params {
@@ -33,34 +34,58 @@ resource "google_compute_instance" "vm_instance" {
   }
 
   network_interface {
-    network = "${google_compute_network.vpc_network.self_link}"
+    network = "default"
     access_config {
     }
   }
 
-  provisioner "remote-exec" {
+  metadata = {
+    ssh-keys = "ubuntu:${chomp(tls_private_key.ssh-key.public_key_openssh)} terraform"
+  }
+
+  tags = ["http-server"]
+
+}
+
+resource "null_resource" "configure-cat-app" {
+  depends_on = [
+    google_compute_instance.hashicat,
+  ]
+
+  triggers = {
+    build_number = timestamp()
+  }
+
+  provisioner "file" {
+    source      = "files/"
+    destination = "/home/ubuntu/"
+
     connection {
       type        = "ssh"
       user        = "ubuntu"
       timeout     = "500s"
       private_key = tls_private_key.ssh-key.private_key_pem
-      host        = self.network_interface.0.access_config.0.nat_ip
+      host        = google_compute_instance.hashicat.network_interface.0.access_config.0.nat_ip
     }
+  }
 
+  provisioner "remote-exec" {
     inline = [
-      "touch /tmp/temp.txt",
+      "sudo add-apt-repository universe",
+      "sudo apt -y update",
+      "sudo apt -y install apache2",
+      "sudo systemctl start apache2",
+      "sudo chown -R ubuntu:ubuntu /var/www/html",
+      "chmod +x *.sh",
+      "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix} ./deploy_app.sh",
     ]
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      timeout     = "500s"
+      private_key = tls_private_key.ssh-key.private_key_pem
+      host        = google_compute_instance.hashicat.network_interface.0.access_config.0.nat_ip
+    }
   }
-
-  tags = ["externalssh"]
-
-  metadata = {
-    ssh-keys = "ubuntu:${tls_private_key.ssh-key.public_key_openssh}"
-  }
-
-}
-
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
 }
---
+++ variables.tf
@@ -0,0 +1,45 @@
+##############################################################################
+# Variables File
+#
+# Here is where we store the default values for all the variables used in our
+# Terraform code. If you create a variable with no default, the user will be
+# prompted to enter it (or define it via config file or command line flags.)
+
+
+variable "project" {
+  description = "Name of your GCP project"
+  default     = "hashicat-gcp-dev"
+}
+variable "prefix" {
+  description = "This prefix will be included in the name of some resources."
+}
+
+variable "region" {
+  description = "The region where the resources are created."
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "The zone where the resources are created."
+  default     = "us-central1-a"
+}
+
+variable "machine_type" {
+  description = "Specifies the GCP instance type."
+  default     = "f1-micro"
+}
+
+variable "height" {
+  default     = "400"
+  description = "Image height in pixels."
+}
+
+variable "width" {
+  default     = "600"
+  description = "Image width in pixels."
+}
+
+variable "placeholder" {
+  default     = "placekitten.com"
+  description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
+}
