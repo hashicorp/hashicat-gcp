35c61b391f475e1e253bd93a86a0a5a99ec3201a
Sean Carolan <scarolan@hashicorp.com>
2020-01-19 15:01:40 -0600

    Update description for project variable.

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@
 # prompted to enter it (or define it via config file or command line flags.)
 
 variable "project" {
-  description = "Name of your GCP project. Make sure you set this up before running this terraform code.  REQUIRED."
+  description = "ID of your GCP project. Make sure you set this up before running this terraform code.  REQUIRED."
 }
 
 variable "prefix" {
