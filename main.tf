2db8b58319910cca41f0da339b6c462656975b94
StanSiska <stanislav.siska@gmail.com>
2020-09-02 12:04:15 +0000

    Updated code

--- main.tf
+++ main.tf
@@ -58,6 +58,11 @@ resource "google_compute_instance" "hashicat" {
 
   tags = ["http-server"]
 
+  labels {
+    department="devops",
+    billable="true"
+  }
+
 }
 
 resource "null_resource" "configure-cat-app" {
