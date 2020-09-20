56ca53e7b9f62c2b8533d3e9912e0f9d46cccbe4
Roger Berlind <roger@hashicorp.com>
2020-09-20 18:48:33 -0400

    remove IP output

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,3 @@
 output "catapp_url" {
   value = "http://${google_compute_instance.hashicat.network_interface.0.access_config.0.nat_ip}"
 }
-
-output "private_ip" {
-  value = google_compute_instance.hashicat.network_interface.0.network_ip
-}
