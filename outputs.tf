f37173eefb236f0b03f3c8b21db1ffbc363f0bf4
rberlind <roger@hashicorp.com>
2020-10-15 19:00:15 -0400

    updated top-level files

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@
 output "catapp_url" {
   value = "http://${google_compute_instance.hashicat.network_interface.0.access_config.0.nat_ip}"
 }
+
+output "catapp_ip" {
+  value = "http://${google_compute_instance.hashicat.network_interface.0.network_ip}"
+}
