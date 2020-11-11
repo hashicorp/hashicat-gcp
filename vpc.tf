7d70e2d4a7662f1c946875208796ee954302e0ae
bpgraham77 <root@p-hhcvnveiyxwn-workstation.c.instruqt-prod.internal>
2020-11-11 20:55:37 +0000

    added corrected vpc

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ module "network" {
   source  = "app.terraform.io/bpg002-training/network/google"
   version = "2.5.0"
   network_name = "bpg002-network"
-  project_id = .var.project
+  project_id = var.project
   subnets = [
   {
     subnet_name   = "bpg002-subnet"
