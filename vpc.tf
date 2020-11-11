28a31cbe14e5041c988a4897af9e5f9b6d804d01
bpgraham77 <root@p-hhcvnveiyxwn-workstation.c.instruqt-prod.internal>
2020-11-11 20:54:23 +0000

    added corrected vpc

--- vpc.tf
+++ vpc.tf
@@ -5,7 +5,7 @@ module "network" {
   project_id = .var.project
   subnets = [
   {
-    subnet_name   = "bpg002--subnet"
+    subnet_name   = "bpg002-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
