2b18061e8a146abcadf3c91bd48e3026951ad650
Micah Miller <micahtmiller@gmail.com>
2020-11-11 20:37:09 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "network" {
+  source  = "app.terraform.io/micah-training/network/google"
+  version = "2.5.0"
+  # insert required variables here
+  network_name = "gaurav-network"
+  project_id = var.project
+
+  subnets = [
+      {
+          subnet_name    = "gaurav-subnet"
+          subnet_ip      = "10.100.10.0/24"
+          subnet_region  = var.region
+      }
+  ]
+}
