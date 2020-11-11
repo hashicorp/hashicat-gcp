2b285aabb713a97fe199a43ae37194f6adda6b7d
bpgraham77 <root@p-hhcvnveiyxwn-workstation.c.instruqt-prod.internal>
2020-11-11 20:52:40 +0000

    added vpc module

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+module "network" {
+  source  = "app.terraform.io/bpg002-training/network/google"
+  version = "2.5.0"
+  network_name = "bpg002-network"
+  project_id = .var.project
+  subnets = [
+  {
+    subnet_name   = "bpg002--subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+] 
+}
