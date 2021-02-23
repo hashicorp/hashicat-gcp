6540e8a218b8fc7616ad1fd6ea844de48bc358e1
Joao A <jotaldeano@gmail.com>
2021-02-23 11:59:20 +0000

    Added the second label

--- main.tf
+++ main.tf
@@ -39,6 +39,7 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
+  billable = true
 
   boot_disk {
     initialize_params {
