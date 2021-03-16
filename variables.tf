d2183f4bf5573f37f2ce5e34c0a6891e0b2e906d
dhaddadi <djamel.haddadi@decathlon.com>
2021-03-16 19:58:48 +0000

    "fix multiple issue !"

--- variables.tf
+++ variables.tf
@@ -47,3 +47,7 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "network_name" {
+  default = "network_hddh"
+}
--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,11 @@
-module "network_vpc" {
+module "vpc" {
   source  = "app.terraform.io/HDDH/network/google//modules/vpc"
   version = "2.5.0"
+
+  project_id = var.project
+  network_name = var.network_name
+
+
   subnets = [
   {
     subnet_name   = "gaurav-subnet"
