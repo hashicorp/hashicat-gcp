835a9efc12c48ecd0b702ec68976ccb89c848443
dhaddadi <djamel.haddadi@decathlon.com>
2021-03-16 20:02:04 +0000

    " pffff "

--- vpc.tf
+++ vpc.tf
@@ -5,12 +5,4 @@ module "vpc" {
   project_id = var.project
   network_name = var.network_name
 
-
-  subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
 }
