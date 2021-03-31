18ec34bfa5a1ad73e5be60623f5c89c46f9a7e5b
Jerem Svn <34279512+Firtha@users.noreply.github.com>
2021-03-31 16:55:35 +0200

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  This is a test, don't contact me for improvements i wont answer.
+  This is a test. Sometimes i meow, sometimes i dont.
   <!-- END -->
 
   </div>
