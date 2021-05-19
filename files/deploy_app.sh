fdce4d430f159f9f8f2ead944f6496e37da182c9
thanraja <45083655+thanraja@users.noreply.github.com>
2021-05-19 12:36:53 -0400

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -12,7 +12,7 @@ cat << EOM > /var/www/html/index.html
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app. 
-  My sweet change
+  Best cat in the world
   <!-- END -->
 
   </div>
