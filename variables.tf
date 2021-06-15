af0e055b6e4f7f17764843965398a3057349d42c
natashahuang <natasha1229uq@gmail.com>
2021-06-15 10:33:16 +0000

    Added vpc module

--- variables.tf
+++ variables.tf
@@ -6,6 +6,7 @@
 # prompted to enter it (or define it via config file or command line flags.)
 
 variable "project" {
+  value="p-l0uwpcs973iasalvwzdcdy88zy35"
   description = "ID of your GCP project. Make sure you set this up before running this terraform code.  REQUIRED."
 }
 
