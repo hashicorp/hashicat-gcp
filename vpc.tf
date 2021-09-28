d6a0a7b62e9685613f043528213609a65fac4dea
Roger Berlind <roger@hashicorp.com>
2021-09-28 12:59:26 -0400

    Update vpc.tf
    Update module version to 3.4.0

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/instruqt-circleci/network/google"
-  version = "3.2.2"
+  version = "3.4.0"
   network_name = "gaurav-network"
   project_id = var.project
   subnets = [
