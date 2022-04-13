78f32b6a9b566c17338c1cd44a773bff757fed96
Andrew <58406293+lackingimagination@users.noreply.github.com>
2022-04-13 17:27:48 -0500

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,32 @@
+
+/**
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+# [START vpc_auto_create]
+resource "google_compute_network" "vpc_network" {
+  project                 = var.project_id
+  name                    = "gaurav-network"
+  auto_create_subnetworks = true
+  mtu                     = 1460
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+}
+# [END vpc_auto_create]
