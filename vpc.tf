eb666c7e0645b395a059ac516bb54b84329d52df
dAmarender <dodleamarenderreddy@gmail.com>
2022-09-21 07:27:54 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,18 +1,62 @@
 module "vpc" {
-  source  = "terraform-google-modules/network/google"
-  version = "4.0"
+    source  = "terraform-google-modules/network/google"
+    version = "~> 4.0"
 
-  project_id   = var.project
-  network_name = "gaurav-network"
-  
-  
+    project_id   = var.project
+    network_name = "gaurav-network"
+    routing_mode = "GLOBAL"
 
-  subnets = [
-    {
-      subnet_name           = "subnet"
-      subnet_ip             = "10.100.10.0/24"
-      subnet_region         = var.project
-      
+    subnets = [
+        {
+            subnet_name           = "gaurav-subnet"
+            subnet_ip             = "10.10.10.0/24"
+            subnet_region         = var.region
+        },
+        {
+            subnet_name           = "subnet-02"
+            subnet_ip             = "10.10.20.0/24"
+            subnet_region         = var.region
+            subnet_private_access = "true"
+            subnet_flow_logs      = "true"
+            description           = "This subnet has a description"
+        },
+        {
+            subnet_name               = "subnet-03"
+            subnet_ip                 = "10.10.30.0/24"
+            subnet_region             = var.region
+            subnet_flow_logs          = "true"
+            subnet_flow_logs_interval = "INTERVAL_10_MIN"
+            subnet_flow_logs_sampling = 0.7
+            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
+        }
+    ]
+
+    secondary_ranges = {
+        subnet-01 = [
+            {
+                range_name    = "subnet-01-secondary-01"
+                ip_cidr_range = "192.168.64.0/24"
+            },
+        ]
+
+        subnet-02 = []
     }
-  ]
+
+    routes = [
+        {
+            name                   = "egress-internet"
+            description            = "route through IGW to access internet"
+            destination_range      = "0.0.0.0/0"
+            tags                   = "egress-inet"
+            next_hop_internet      = "true"
+        },
+        {
+            name                   = "app-proxy"
+            description            = "route through proxy to reach app"
+            destination_range      = "10.50.10.0/24"
+            tags                   = "app-proxy"
+            next_hop_instance      = "app-proxy-instance"
+            next_hop_instance_zone = var.region
+        },
+    ]
 }
