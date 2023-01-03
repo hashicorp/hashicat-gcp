c1dee95ee13a1160b7b3d78d3119d2724702781c
ameyabak0609 <119598201+ameyabak0609@users.noreply.github.com>
2023-01-03 11:23:15 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,6 +9,10 @@ variable "project" {
   description = "ID of your GCP project. Make sure you set this up before running this terraform code.  REQUIRED."
 }
 
+variable "prefix" {
+  description = "This prefix will be included in the name of some resources. You can use your own name or any other short string here."
+}
+
 variable "region" {
   description = "The region where the resources are created."
   default     = "us-central1"
