7026af8ff4cb24b2b0a21e89109c369566902e34
Your Name <you@example.com>
2023-02-22 18:07:18 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,12 @@
-network_name ="gaurav-network"
+credentials "app.terraform.io" {
+    # valid user API token:
+    token = "57zVYaZhZwfYdw.atlasv1.CXh6WJ10KHgxy9TxAWy3E3Vrybga1CH5ppdpPaAINOeRqdqszA7uxr1dOtM4BHX98GI"
+  }
+
+module "network" {
+  source  = "app.terraform.io/hashicat-gcp-handson/network/google"
+  version = "6.0.1"
+  network_name ="gaurav-network"
 project_id = "var.project"
 subnets = [
   {
@@ -7,3 +15,6 @@ subnets = [
     subnet_region = var.region
   }
 ]
+}
+
+
