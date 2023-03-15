170ef65c22564246c6b1c0f22f88129c5a55aee9
ByungSeokLEE <89229875+ByungSeokLEE@users.noreply.github.com>
2023-03-15 14:10:41 +0900

    Update vpc.tf
    12312

--- vpc.tf
+++ vpc.tf
@@ -6,7 +6,7 @@ variable "network_project_id" {}
 // Modules
 module "network" {
   source  = "app.terraform.io/bslee/network/google"
-  version = "6.0.1"
+  version = "3.5.0"
 
   network_name = "bslee1-vpc-us-east1"
   project_id = "${var.network_project_id}"
