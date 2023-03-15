e45083f663d71ead037b39044edca57f659b1de6
ByungSeokLEE <89229875+ByungSeokLEE@users.noreply.github.com>
2023-03-15 14:22:29 +0900

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -10,5 +10,11 @@ module "network" {
 
   network_name = "bslee1-vpc-us-east1"
   project_id = "123"
-  subnets = "10.100.10.0/24"
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
 }
