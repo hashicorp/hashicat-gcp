66d518f35f391ef4c3c6eea991a573c16ca732b9
Nathaniel McDowell <nat@blueii.co.uk>
2023-05-12 00:25:02 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 3.0.4"
+
+    project_id   = var.project 
+    network_name = "rebeard-network"
+
+    subnets = [
+        {
+            subnet_name           = "redbeard-subnet-01"
+            subnet_ip             = "10.100.10.0/24"
+            subnet_region         = var.region 
+        }
+    ]
+}
