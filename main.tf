fc18655e238775a4be4e572bc79a803e795e9ea2
Nathaniel McDowell <nat@blueii.co.uk>
2023-05-12 00:33:08 +0000

    Added vpc module

--- main.tf
+++ main.tf
@@ -50,8 +50,6 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-  department   = "devops"
-  billable     = "true"
 
 
   boot_disk {
