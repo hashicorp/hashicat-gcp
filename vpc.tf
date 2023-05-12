25c5701802ab29ed88484bd733a9059eaac241db
Nathaniel McDowell <nat@blueii.co.uk>
2023-05-12 01:30:30 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,17 @@
 module "vpc" {
-    source  = "terraform-google-modules/network/google//modules/vpc" 
-    version = "~> 3.4.0"
+    source  = "terraform-google-modules/network/google" 
+    version = "3.4.0"
 
     project_id   = var.project 
-    network_name = "redbeard-network-2"
+    network_name = "redbeard-network-03"
 
     shared_vpc_host = false
+
+    subnets = [
+        {   
+            subnet_name           = "redbeard-subnet-02"
+            subnet_ip             = "10.100.10.0/24"
+            subnet_region         = var.region
+        }
+    ]
 }
