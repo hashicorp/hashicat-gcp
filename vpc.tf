4b72a0dd8ad50ca4f8f68b78a448a8c65b099c83
Nathaniel McDowell <nat@blueii.co.uk>
2023-05-12 03:16:11 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,14 +1,14 @@
 module "network_vpc" {
-    source  = "terraform-google-modules/network/google"
+    source  = "terraform-google-modules/network/google//modules/vpc"
     version = "~> 3.0"
 
     project_id   = var.project 
-    network_name = "redbeard-network-06"
+    network_name = "redbeard-network-07"
     routing_mode = "GLOBAL"
 
     subnets = [
         {
-            subnet_name           = "redbeard-subnet-06"
+            subnet_name           = "redbeard-subnet-07"
             subnet_ip             = "10.100.10.0/24"
             subnet_region         = var.region
         }
