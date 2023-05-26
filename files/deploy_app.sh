c1009a1b41cac9af3ec2287963c64d8225ea1376
hashi-shaohern <100564490+hashi-shaohern@users.noreply.github.com>
2023-05-26 14:30:26 +0800

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -13,7 +13,7 @@ cat << EOM > /var/www/html/index.html
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
+  <center><h2>Welcome to ${PREFIX}'s app. Replace this text with your own.</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own.
   <!-- END -->
 
