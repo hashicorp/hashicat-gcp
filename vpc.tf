4e19d718e27549e615da42e836d8785f337c3a82
jimmy31120 <jeremie.dulac@outlook.com>
2023-09-04 14:42:46 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,10 @@
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
-
-    project_id   = var.project
-    network_name = "gaurav-network"
-    routing_mode = "GLOBAL"
-
-    subnets = [
+module "network" {
+  source  = "app.terraform.io/Jimorg/network/google"
+  version = "3.4.0"
+  project_id   = var.project
+  network_name = "gaurav-network"
+  routing_mode = "GLOBAL"
+  subnets = [
       {
         subnet_name   = "gaurav-subnet"
         subnet_ip     = "10.100.10.0/24"
